f33e6915b2040bd4a4260cc203bdf9d3c67596ce
pbadun <pbadun@gmail.com>
2022-05-02 16:30:40 +0300

    Initial commit

---
+++ src/main/java/com/bb8qq/game/AppZmeika.java
@@ -0,0 +1,44 @@
+package com.bb8qq.game;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Hello world!
+ */
+public class AppZmeika {
+    private static boolean bQuit = false;
+    private JFrame frame;
+    private ScreenView main;
+
+
+    public static void main(String[] args) {
+        new AppZmeika();
+    }
+
+    public AppZmeika() {
+        this.frame = new JFrame();
+        this.main = new ScreenView(frame);
+        frame.add(main);
+
+        frame.setSize(1024, 1024);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                bQuit = true;
+                System.exit(0);
+            }
+        });
+        frame.setVisible(true);
+
+        while (!bQuit) {
+            try {
+                Thread.sleep(100l);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/bb8qq/game/ScreenView.java
@@ -0,0 +1,15 @@
+package com.bb8qq.game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ScreenView extends JPanel {
+    private Frame frame;
+
+    public ScreenView(Frame frame) {
+        this.frame = frame;
+        //addMouseListener(this);
+        //addMouseMotionListener(this);
+        //frame.addKeyListener(new KeyInputHandler());
+    }
+}
